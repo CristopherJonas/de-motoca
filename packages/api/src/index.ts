40c5f06047daef2d316b5ba6bb06afec00e92161
Cristopher Jonas de Faria <cristopherjonasfaria@gmail.com>
2020-04-04 14:30:27 -0300

    install KOA

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,1 +1,9 @@
-console.log('dfsdf');
+import router from './routes';
+
+import Koa = require('koa');
+
+const app = new Koa();
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+app.listen(8000);
