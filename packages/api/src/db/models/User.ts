4a5d766f7f4f5ca6427a4b98344733a67edcf227
Cristopher Jonas de Faria <cristopherjonasfaria@gmail.com>
2020-04-11 15:44:57 -0300

    use primitive types

--- packages/api/src/db/models/User.ts
+++ packages/api/src/db/models/User.ts
@@ -1,10 +1,10 @@
 import { Model, Sequelize, DataTypes, BuildOptions } from 'sequelize';
 
 interface UserModel extends Model {
-  readonly id: String;
-  name: String;
-  email: String;
-  password_hash: String;
+  readonly id: string;
+  name: string;
+  email: string;
+  password_hash: string;
   created_at: Date;
   updated_at: Date;
 }
