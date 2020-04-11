c1380893df9bd3e86029d8e969774f04c02a1e47
Cristopher Jonas de Faria <cristopherjonasfaria@gmail.com>
2020-04-11 15:45:10 -0300

    custom error handler

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -8,6 +8,15 @@ import Koa = require('koa');
 const app = new Koa();
 app.use(Bodyparser());
 app.use(errorHandler());
+app.use(async (_ctx, next) => {
+  try {
+    const response = await next();
+    return response;
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error(error);
+  }
+});
 app.use(router.routes());
 app.use(router.allowedMethods());
 app.listen(8000);
