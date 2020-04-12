5ae9d2309b6a5b8ca9fd1e720df5b254a4a949ed
Cristopher Jonas de Faria <cristopherjonasfaria@gmail.com>
2020-04-12 15:46:20 -0300

    importing custom middlewares

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,5 +1,7 @@
 import { errorHandler } from 'graphql-api-koa';
 import router from './routes';
+import { customError } from './middlewares/customError';
+import { auth } from './middlewares/auth';
 
 const Bodyparser = require('koa-bodyparser');
 
@@ -8,15 +10,8 @@ import Koa = require('koa');
 const app = new Koa();
 app.use(Bodyparser());
 app.use(errorHandler());
-app.use(async (_ctx, next) => {
-  try {
-    const response = await next();
-    return response;
-  } catch (error) {
-    // eslint-disable-next-line no-console
-    console.error(error);
-  }
-});
+app.use(customError);
+app.use(auth);
 app.use(router.routes());
 app.use(router.allowedMethods());
 app.listen(8000);
