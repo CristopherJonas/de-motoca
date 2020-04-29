67a6bd37a1ab61083d6b87b48f440aa15de8405b
Cristopher Jonas de Faria <cristopherjonasfaria@gmail.com>
2020-04-28 21:52:20 -0300

    refactor, now using generated types

--- packages/api/src/graphql/modules/auth/auth.resolvers.ts
+++ packages/api/src/graphql/modules/auth/auth.resolvers.ts
@@ -1,8 +1,7 @@
 import { promisify } from 'util';
 import { compare } from 'bcrypt';
 import { sign, SignOptions, Secret } from 'jsonwebtoken';
-
-import { GraphQLContext } from '../../context';
+import { IResolvers } from '../../generated/schema';
 
 const signAsync = promisify<
   string | Buffer | object,
@@ -11,51 +10,41 @@ const signAsync = promisify<
   string
 >(sign);
 
-type loginInput = {
-  email: string;
-  password: string;
-  clientMutationId?: string | null;
-};
-
-const login = async (
-  _parent: {},
-  args: { input: loginInput },
-  ctx: GraphQLContext,
-) => {
-  const user = await ctx.models.User.findOne({
-    where: {
-      email: args.input.email.toLowerCase(),
-    },
-  });
-  if (!user) {
-    return {
-      clientMutationId: args.input.clientMutationId,
-      error: ['Email ou password incorretos'],
-    };
-  }
-  const valid = await compare(args.input.password, user.password_hash);
-  if (!valid) {
-    return {
-      clientMutationId: args.input.clientMutationId,
-      error: ['Email ou password incorretos'],
-    };
-  }
-  const token = await signAsync(
-    { userId: user.id },
-    process.env.API_SECRET as string,
-    {
-      expiresIn: '1d',
-    },
-  );
-  return {
-    clientMutationId: args.input.clientMutationId,
-    error: [''],
-    token,
-  };
-};
-
-export default {
+const resolvers: IResolvers = {
   Mutation: {
-    login,
+    login: async (_parent, args, ctx) => {
+      const user = await ctx.models.User.findOne({
+        where: {
+          email: args.input.email.toLowerCase(),
+        },
+      });
+      if (!user) {
+        return {
+          clientMutationId: args.input.clientMutationId,
+          error: ['Email ou password incorretos'],
+        };
+      }
+      const valid = await compare(args.input.password, user.password_hash);
+      if (!valid) {
+        return {
+          clientMutationId: args.input.clientMutationId,
+          error: ['Email ou password incorretos'],
+        };
+      }
+      const token = await signAsync(
+        { userId: user.id },
+        process.env.API_SECRET as string,
+        {
+          expiresIn: '1d',
+        },
+      );
+      return {
+        clientMutationId: args.input.clientMutationId,
+        error: [''],
+        token,
+      };
+    },
   },
 };
+
+export default resolvers;
